bba907851bcc3b5aaeade8a9af3ccd7f6f436ef2
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-08 16:29:42 +0300

    Creates quotes-form component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
 import { QuotesDetailComponent } from './components/quotes-detail/quotes-detail.component';
+import { QuotesFormComponent } from './components/quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailComponent
+    QuotesDetailComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
