132c89a2a09271025162303bb543da9e506cc20a
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-09 10:38:26 +0300

    Add a division for form in quotes template.

--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
 
-    constructor(public title:string, public qouteInfo:string, public author:string, public datePosted:Date, public upvote:number, public downvote:number, public postedBy:string) {
+    constructor(public title: string, public quoteInfo: string, public author: string, public datePosted: Date, public upvote: number, public downvote: number, public postedBy: string) {
 
     }
 }
