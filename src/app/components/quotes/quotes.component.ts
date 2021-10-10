96e2435fee9e3979261a4fe5d6033003a7a16f88
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-10 13:01:55 +0300

    Change names of people who post quotes as well as time.

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import { Quotes } from '../../models/quotes';
 export class QuotesComponent implements OnInit {
 
   quotes:Quotes[] = [
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,10,8), 0, 0, 'Ngetich'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,10,8), 0, 0, 'Ngetich'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,10,8), 0, 0, 'Ngetich'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,10,8), 0, 0, 'Ngetich')
+    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,5,18), 0, 0, 'Ngetich'),
+    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,6,12), 0, 0, 'Gideon'),
+    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,7,11), 0, 0, 'Maxwell'),
+    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,8,22), 0, 0, 'Yuri')
   ]
 
   removeQuote(isRead: any, index:number) {
