618730431a224055ee39b07f921156337ed38e90
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-11 00:45:47 +0300

    Remove Lorem text and replace it with actual quotes plus some styles.

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import { Quotes } from '../../models/quotes';
 export class QuotesComponent implements OnInit {
 
   quotes:Quotes[] = [
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,5,18), 0, 0, 'Ngetich'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,6,12), 0, 0, 'Gideon'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,7,11), 0, 0, 'Maxwell'),
-    new Quotes('Philosophy', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Anonymous', new Date(2021,8,22), 0, 0, 'Yuri')
+    new Quotes('Life', '“Many of life’s failures are people who did not realize how close they were to success when they gave up.”', 'Thomas A. Edison', new Date(2021,5,18), 0, 0, 'Ngetich'),
+    new Quotes('Philosophy', '“Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.”', 'Buddha', new Date(2021,6,12), 0, 0, 'Gideon'),
+    new Quotes('Inspirational', '“Inspiration comes from within yourself. One has to be positive. When you are positive, good things happen.”', 'Deep Roy', new Date(2021,7,11), 0, 0, 'Maxwell'),
+    new Quotes('Love', '“There are no monuments dedicated to me and my name will soon be forgotten, but I’ve loved another with all my heart and soul, and to me, this has always been enough.”', 'Nicholas Sparks', new Date(2021,8,22), 0, 0, 'Yuri')
   ]
 
   removeQuote(isRead: any, index:number) {
